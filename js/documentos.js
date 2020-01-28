d3f97e14fdf16d987b1c0e0761b0889cd10a6df7
Luana <luana_limas@hotmail.com>
2020-01-28 20:50:35 -0300

    Busca de pasta com cpf no firebase

---
+++ js/documentos.js
@@ -0,0 +1,30 @@
+function search(){
+    var cpf = document.getElementById('inputCPF').value;
+    validation(cpf);
+}
+function validation(cpfValue){
+    var storage = firebase.storage();
+    //Retorna uma promisse que será processada
+    var resultado = storage.ref().child(cpfValue).listAll().then(function(todosArquivos){
+        if(todosArquivos.items.length >= 1){
+            next(cpfValue);
+        } else {
+            alert('CPF Não Cadastrado');
+        }
+        
+    })
+    .catch(function(error){
+        console.log('Erro', error);
+    });
+}
+function next(cpfValue){
+    document.getElementById('busca').setAttribute("class", "ocultar");
+    document.getElementById('resultado').removeAttribute("class", "ocultar");
+    document.getElementById('tituloDocumentos').innerHTML = 'Certificados de: '+cpfValue;
+}
+function back(){
+    document.getElementById('busca').removeAttribute("class", "ocultar");
+    document.getElementById('resultado').setAttribute("class", "ocultar");
+    document.getElementById('inputCPF').value = '';
+}
+
---
+++ js/initFirebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB_VL0E4t3xRKBehxM21z0Rjln8PU03CXU",
+    authDomain: "documentos-dbb05.firebaseapp.com",
+    databaseURL: "https://documentos-dbb05.firebaseio.com",
+    projectId: "documentos-dbb05",
+    storageBucket: "documentos-dbb05.appspot.com",
+    messagingSenderId: "829369648632",
+    appId: "1:829369648632:web:2f57fa86ed95bee2ce420d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
